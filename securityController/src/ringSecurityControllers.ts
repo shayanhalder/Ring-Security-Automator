751800c10e5e89dc71f94a9435eb7c87ad0e3af3
Shayan Halder <shayanhalder2389@gmail.com>
2026-06-25 21:05:09 -0700

    fix variable name

--- securityController/src/ringSecurityControllers.ts
+++ securityController/src/ringSecurityControllers.ts
@@ -74,9 +74,11 @@ export async function disarmSecurity(page: Page, password: string, accountDashbo
 }
 
 export async function armSecurityHome(page: Page, password: string, accountDashboardURL: string) {
-    const armSecurityHomeBUtton = page.locator('div[aria-label="To arm & set to home mode, press this button."]');
-    await armSecurityHomeBUtton.waitFor();
-    await armSecurityHomeBUtton.click();
+    const armSecurityHomeButton = page.locator('div[aria-label="To arm & set to home mode, press this button."]');
+    await armSecurityHomeButton.waitFor();
+    await armSecurityHomeButton.click();
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
 
     const reloginPrompt = await checkReloginPrompt(page);
 
@@ -92,9 +94,9 @@ export async function armSecurityHome(page: Page, password: string, accountDashb
         const currentUrl = page.url();
         console.log('Current URL after navigation:', currentUrl);
 
-        const armSecurityHomeBUtton = page.locator('div[aria-label="To arm & set to away mode, press this button."]');
-        await armSecurityHomeBUtton.waitFor();
-        await armSecurityHomeBUtton.click();
+        const armSecurityHomeButton = page.locator('div[aria-label="To arm & set to away mode, press this button."]');
+        await armSecurityHomeButton.waitFor();
+        await armSecurityHomeButton.click();
     }
 
     const backToDashboardButton = page.getByRole('button', { name: 'Back to Dashboard' });
